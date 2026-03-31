030593677037c8dcb116bb998f3d7c73b4d72cb2
basil3691 <elispaulsen@gmail.com>
2026-03-31 10:54:39 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2174,7 +2174,7 @@ function renderMenu(menu: Exclude<OpenMenu, null>) {
     display: "grid",
     gridTemplateColumns: "minmax(0, 1fr) 110px",
     alignItems: "center",
-    columnGap: 25,
+    columnGap: 3,
     marginBottom: 12,
     position: "sticky",
     top: 0,
